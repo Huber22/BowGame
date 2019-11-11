1e17e37352a8853601277f581a33242f9209856e
Huber22 <nickhuber22@gmail.com>
2019-11-11 16:19:09 -0600

    First commit

---
+++ Bow Game/Assets/ShootArrow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootArrow : MonoBehaviour
+{
+    public float arrowforce;
+    public GameObject arrowPrefab;
+    public GameObject firePoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)){
+            Instantiate(arrowPrefab, firePoint.transform.position, Quaternion.identity);
+        }
+    }
+}
